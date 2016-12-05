ecad023f7a27120373b7eac789436090e5d02fce
kellerd <danny.keller@tc.gc.ca>
2016-12-05 14:19:45 -0500

    Start Day5

---
+++ Day5/Program.fsx
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+#r "bin/Debug/Library.dll"
+
+open Advent.Library
+open System.IO
+open System
+open System.Security.Cryptography
+let md5 = MD5.Create()
+let x = [1..100000] |> List.map (string >> System.Text.Encoding.ASCII.GetBytes)
+List.map (fun (buffer:byte[]) -> md5.ComputeHash(buffer)) x
