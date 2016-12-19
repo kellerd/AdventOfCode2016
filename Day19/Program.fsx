8d08dc104a65778124ac41ca96eb8151e7502e05
kellerd <danny.keller@tc.gc.ca>
2016-12-19 15:38:10 -0500

    Day19

---
+++ Day19/Program.fsx
@@ -0,0 +1,22 @@
+﻿open System.Collections.Generic
+open System.Linq
+let elfShuffle1 n =
+    let linkedList = System.Collections.Generic.LinkedList(Seq.init n ((+) 1))
+
+    let nextOrLast (current:LinkedListNode<'T> ) =  
+        match current.Next with
+        | null -> current.List.First
+        | x -> x
+    let mutable current = linkedList.First
+    while linkedList.Count > 1 do
+        linkedList.Remove(nextOrLast current)
+        current <- nextOrLast current
+    current.Value
+
+//elfShuffle1 5
+//elfShuffle1 6
+//elfShuffle1 3017957
+
+// Make the supplied sequence appear circular but with
+// at most 'n' elements, n = -1 elicits infinite repetition.
+
